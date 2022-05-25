6d528d1a6193683bb18869f725d28218f46971e7
nicolas_aponte <ste.nicolas.ap@gmail.com>
2022-05-24 21:11:47 -0500

    feat: Se crea proyecto base de customer service.

---
+++ customer-service/src/main/java/com/example/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.example.customerservice.repository;
+
+
+import com.example.customerservice.repository.entity.Customer;
+import com.example.customerservice.repository.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository  extends JpaRepository<Customer,Long> {
+        public Customer findByNumberID(String numberID);
+        public List<Customer> findByLastName(String lastName);
+        public List<Customer> findByRegion(Region region);
+}
