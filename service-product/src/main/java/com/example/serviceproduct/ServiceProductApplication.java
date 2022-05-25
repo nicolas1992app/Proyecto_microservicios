224d5e8c68edc2c464cb6bb55c3aa25b5315f45e
nicolas_aponte <ste.nicolas.ap@gmail.com>
2022-05-25 15:58:12 -0500

    feat: Se añade configuración de eureka a los microservicios clientes. Se añade configuracion a customer y product y shopping service.

--- service-product/src/main/java/com/example/serviceproduct/ServiceProductApplication.java
+++ service-product/src/main/java/com/example/serviceproduct/ServiceProductApplication.java
@@ -3,10 +3,11 @@ package com.example.serviceproduct;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
